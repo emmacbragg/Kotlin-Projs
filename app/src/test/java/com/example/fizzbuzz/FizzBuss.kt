64c3bbf4d914b8290b919cacfbc74852122ed43f
emmacbragg <67954195+emmacbragg@users.noreply.github.com>
2020-07-07 15:11:46 +0100

    Update FizzBuss.kt

--- app/src/test/java/com/example/fizzbuzz/FizzBuss.kt
+++ app/src/test/java/com/example/fizzbuzz/FizzBuss.kt
@@ -1,36 +1,78 @@
 package com.example.fizzbuzz
+import java.lang.NumberFormatException
 
 fun main() {
-    fizzBuzz()
+    println("What value would you like to FizzBuzz up to, please enter a number?")
+    var max: Int
+    try {
+        max = Integer.valueOf(readLine())
+    }
+    catch (e:NumberFormatException){
+        println("Error : Input not numerical, setting maximum value to 500")
+        max = 500
+    }
+
+    println()
+
+    println("Which rules would you like to apply?" +
+            "\nEnter your choices as a list only seperated by spaces" +
+            "\nOptions are 3, 5, 7, 11, 13 and 17")
+
+    val input: String? = readLine()
+    var rules: List<String>? = input?.split(" ".toRegex())
+
+    val allowed = listOf("3", "5", "7", "11", "13", "17")
+
+    for (rule in rules!!){
+        if (rule !in allowed){
+            println("Error: '" + rule + "' is not an allowed rule" +
+                    "\nApplying all rules")
+            rules = listOf("3","5","7","11","13","17")
+        }
+    }
+
+    fizzBuzz(max, rules)
 }
 
-fun fizzBuzz() {
+fun fizzBuzz(max : Int, rules : List<String>?) {
     var index = 1
-    var FB = 0
-    while (index <= 100) {
-        if (index.rem(11) == 0) {
-            print("Bong")
-            FB = 1
+    var printLst = mutableListOf<String>()
+    println()
+
+    while (index <= max) {
+        if (rules != null) {
+            if (rules.contains("3") && index.rem(3) == 0 && index.rem(11) != 0) {
+                printLst.add("Fizz")
+            }
+            if (rules.contains("11") && index.rem(13) == 0) {
+                printLst.add("Fezz")
+            }
+            if (rules.contains("11") && index.rem(11) == 0) {
+                printLst.add("Bong")
+            }
+            if (rules.contains("5") && index.rem(5) == 0 && index.rem(11) != 0){
+                printLst.add("Buzz")
+            }
+            if (rules.contains("7") && index.rem(7) == 0 && index.rem(11) != 0){
+                printLst.add("Bang")
+            }
+            if (index.rem(17) == 0){
+                printLst.reverse()
+            }
         }
-        else {
-        if (index.rem(3) == 0) {
-            print("Fizz")
-            FB = 1
-        }
-        if (index.rem(5) == 0){
-            print("Buzz")
-            FB = 1
-        }
-        if (index.rem(7) == 0){
-            print("Bang")
-            FB = 1
-        }
-        }
-        if (FB == 0){
+
+        if (printLst.isEmpty()){
             print(index)
         }
-        FB = 0
+
+        else{
+            for (item in printLst) {
+                print(item)
+            }
+        }
+
         println()
+        printLst = mutableListOf<String>()
         index++
     }
 }
